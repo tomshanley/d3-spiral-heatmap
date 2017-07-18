81dc8ea25e1bca861c832f0dd5eecc97622e56a3
Tom Shanley <tom.shanley@gmail.com>
2017-07-19 08:52:17 +1200

    Renamed segments to arcs and updated function to label arcs using values from the dataset rather than user defined array

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ When the chart is called on a selection, a spiral heatmap is created within a "g
 
     selection.call(chart)
 
-Each arc (or segments) of the spiral is path element contained within a "g" element with the class ".arc", which can be selected and styled after it is created. For example:
+Each arc of the spiral is path element contained within a "g" element with the class ".arc", which can be selected and styled after it is created. For example:
 
     g.selectAll(".arc")
         .selectAll("path")
@@ -44,20 +44,20 @@ The radius (r) of the spiral chart. The radius is from the centre of the chart t
 
 The proportion (p) of the radius which will be left as a hole in the centre of the chart. Must be between 0 and 1.
 
-0 will mean the spiral starts at the centre of the chart, which produces pointed segments. Default is 0.3.
+0 will mean the spiral starts at the centre of the chart, which produces pointed arcs at the centre. Default is 0.3.
 
-*chart*.**segmentsPerCoil**(s)
+*chart*.**arcsPerCoil**(s)
 
-The number of segments per coil of the spiral. Usually this is set to match the number of intervals in the period you are charting, eg <b>12</b> months per year, <b>24</b> hours per day, etc. Default is 12.
+The number of arcs per coil of the spiral. Usually this is set to match the number of intervals in the period you are charting, eg <b>12</b> months per year, <b>24</b> hours per day, etc. Default is 12.
 
 *chart*.**coilPadding**(p)
 
-The proportion (0 to 1) of the coil width that will be used as padding between coils. Padding is useful if you want to highlight the spiral rather than the segments. Must be between 0 and 1. Default is 0 (no padding).
+The proportion (0 to 1) of the coil width that will be used as padding between coils. Padding is useful if you want to highlight the spiral rather than the segments of arcs. Must be between 0 and 1. Default is 0 (no padding).
 
-*chart*.**segmentLabels**(array)
+*chart*.**arcLabel**(fieldname)
 
-An array of labels that will be placed around the circumference of the spiral. Typically you would have the same number of values in the array as segmentsPerCoil. Default is an empty array (no labels).
+The field in the data that will label each segment of arcs in the spiral. The label will have the class ".arc-label" for selection and styling. Defaults to no field (no labels).
 
-*chart*.**coilLabels**(fieldname)
+*chart*.**coilLabel**(fieldname)
 
-The field in the data that will label each coil. The label will have the class ".coil-label" for selection and styling. Defaults to null (no labels).
+The field in the data that will label each coil. The label will have the class ".coil-label" for selection and styling.  Defaults to no field (no labels).
--- d3-spiral-heatmap.js
+++ d3-spiral-heatmap.js
@@ -2,273 +2,279 @@
 // The following options are available
 // radius: radius of the overall plot, not including any labels. Default 250
 // holeRadiusProportion: the proportion (0 to 1) of the radius (see above) that is left as a hole. Default 0.
-// segmentsPerCoil: a 'coil' is one revolution of the spiral.  This sets how many segments (or arcs) you want per coil. Typically this would
+// arcsPerCoil: a 'coil' is one revolution of the spiral.  This sets how many arcs (or arcs) you want per coil. Typically this would
 //                  be set according to the periodicity of the data. For example, 12 for months per year, 24 for hours per day, etc
 // coilPadding: the proportion (0 to 1) of the coil width that is used for padding between coils. Useful for making the spiral very noticeable
-// segmentLabels: an array of labels that will be placed around the circumference of the spiral
+// arcLabel: the field name to use for the labels around the circumference
+// coilLabel: the field name to use for the labels at the beginning of each coil
 
-function spiralHeatmap() {
-  
+function spiralHeatmap () {
   // constants
-  const radians = 0.0174532925;
+  const radians = 0.0174532925
 
   // All options that are accessible to caller
   // Default values
-  var radius = 250;
-  var holeRadiusProportion = 0.3; //proportion of radius
-  var segmentsPerCoil = 12; //assuming months per year
-  var coilPadding = 0; //no padding
-  var segmentLabel = []; //no labels
-  var coilLabel = ""; //no labels
+  var radius = 250
+  var holeRadiusProportion = 0.3 // proportion of radius
+  var arcsPerCoil = 12 // assuming months per year
+  var coilPadding = 0 // no padding
+  var arcLabel = '' // no labels
+  var coilLabel = '' // no labels
 
-  function chart(selection) {
-    selection.each(function(data) {
-      const segmentAngle = 360 / segmentsPerCoil;
-      const labelRadius = radius + 20;
+  function chart (selection) {
+    selection.each(function (data) {
+      const arcAngle = 360 / arcsPerCoil
+      const labelRadius = radius + 20
+
+      var arcLabelsArray = []
+
+      for (var i = 1; i <= arcsPerCoil; i++) {
+        arcLabelsArray.push(i)
+      }
+
+
 
       // Create/update the x/y coordinates for the vertices and control points for the paths
       // Stores the x/y coordinates on the data
-      updatePathData(data);
+      updatePathData(data)
 
       let thisSelection = d3
         .select(this)
-        .append("g")
-        .attr("class", "spiral-heatmap");
+        .append('g')
+        .attr('class', 'spiral-heatmap')
 
-      var segmentLabelsG = thisSelection
-        .selectAll(".segment-label")
-        .data(segmentLabel)
+      var arcLabelsG = thisSelection
+        .selectAll('.arc-label')
+        .data(arcLabelsArray)
         .enter()
-        .append("g")
-        .attr("class", "segment-label");
+        .append('g')
+        .attr('class', 'arc-label')
 
-      segmentLabelsG
-        .append("text")
-        .text(function(d) {
-          return d;
+      arcLabelsG
+        .append('text')
+        .text(function (d) {
+          return data[d][arcLabel]
         })
-        .attr("x", function(d, i) {
-          let labelAngle = i * segmentAngle + segmentAngle / 2;
-          return x(labelAngle, labelRadius);
+        .attr('x', function (d, i) {
+          let labelAngle = i * arcAngle + arcAngle / 2
+          return x(labelAngle, labelRadius)
         })
-        .attr("y", function(d, i) {
-          let labelAngle = i * segmentAngle + segmentAngle / 2;
-          return y(labelAngle, labelRadius);
+        .attr('y', function (d, i) {
+          let labelAngle = i * arcAngle + arcAngle / 2
+          return y(labelAngle, labelRadius)
         })
-        .style("text-anchor", function(d, i) {
-          return i < segmentLabel.length / 2 ? "start" : "end";
-        });
+        .style('text-anchor', function (d, i) {
+          return i < arcLabelsArray.length / 2 ? 'start' : 'end'
+        })
 
-      segmentLabelsG
-        .append("line")
-        .attr("x2", function(d, i) {
-          let lineAngle = i * segmentAngle;
-          let lineRadius = chartRadius + 10;
-          return x(lineAngle, lineRadius);
+      arcLabelsG
+        .append('line')
+        .attr('x2', function (d, i) {
+          let lineAngle = i * arcAngle
+          let lineRadius = chartRadius + 10
+          return x(lineAngle, lineRadius)
         })
-        .attr("y2", function(d, i) {
-          let lineAngle = i * segmentAngle;
-          let lineRadius = chartRadius + 10;
-          return y(lineAngle, lineRadius);
-        });
+        .attr('y2', function (d, i) {
+          let lineAngle = i * arcAngle
+          let lineRadius = chartRadius + 10
+          return y(lineAngle, lineRadius)
+        })
 
       var arcs = thisSelection
-        .selectAll(".arc")
+        .selectAll('.arc')
         .data(data)
         .enter()
-        .append("g")
-        .attr("class", "arc");
+        .append('g')
+        .attr('class', 'arc')
 
-      arcs.append("path").attr("d", function(d) {
+      arcs.append('path').attr('d', function (d) {
         // start at vertice 1
-        let start = "M " + d.x1 + " " + d.y1;
+        let start = 'M ' + d.x1 + ' ' + d.y1
         // inner curve to vertice 2
         let side1 =
-          " Q " +
+          ' Q ' +
           d.controlPoint1x +
-          " " +
+          ' ' +
           d.controlPoint1y +
-          " " +
+          ' ' +
           d.x2 +
-          " " +
-          d.y2;
+          ' ' +
+          d.y2
         // straight line to vertice 3
-        let side2 = "L " + d.x3 + " " + d.y3;
+        let side2 = 'L ' + d.x3 + ' ' + d.y3
         // outer curve vertice 4
         let side3 =
-          " Q " +
+          ' Q ' +
           d.controlPoint2x +
-          " " +
+          ' ' +
           d.controlPoint2y +
-          " " +
+          ' ' +
           d.x4 +
-          " " +
-          d.y4;
+          ' ' +
+          d.y4
         // combine into string, with closure (Z) to vertice 1
-        return start + " " + side1 + " " + side2 + " " + side3 + " Z";
-      });
+        return start + ' ' + side1 + ' ' + side2 + ' ' + side3 + ' Z'
+      })
 
-      //create coil labels on the first arc of each coil
-      console.log(coilLabel)
+      // create coil labels on the first arc of each coil
       coilLabels = arcs
-        .filter(function(d) {
-          return d.arcNumber == 0;
+        .filter(function (d) {
+          return d.arcNumber == 0
         })
-        .raise();
+        .raise()
 
       coilLabels
-        .append("path")
-        .attr("id", function(d) {
-          console.log(d.year)
-          return "path-" + d[coilLabel];
+        .append('path')
+        .attr('id', function (d) {
+          return 'path-' + d[coilLabel]
         })
-        .attr("d", function(d) {
-          //start at vertice 1
-          let start = "M " + d.x1 + " " + d.y1;
-          //inner curve to vertice 2
+        .attr('d', function (d) {
+          // start at vertice 1
+          let start = 'M ' + d.x1 + ' ' + d.y1
+          // inner curve to vertice 2
           let side1 =
-            " Q " +
+            ' Q ' +
             d.controlPoint1x +
-            " " +
+            ' ' +
             d.controlPoint1y +
-            " " +
+            ' ' +
             d.x2 +
-            " " +
-            d.y2;
-          return start + side1;
+            ' ' +
+            d.y2
+          return start + side1
         })
-        .style("opacity", 0);
+        .style('opacity', 0)
 
       coilLabels
-        .append("text")
-        .attr("class", "coil-label")
-        .attr("x", 3)
-        .attr("dy", -4)
-        .append("textPath")
-        .attr("xlink:href", function(d) {
-          return "#path-" + d[coilLabel];
+        .append('text')
+        .attr('class', 'coil-label')
+        .attr('x', 3)
+        .attr('dy', -4)
+        .append('textPath')
+        .attr('xlink:href', function (d) {
+          return '#path-' + d[coilLabel]
         })
-        .text(function(d) {
-          return d[coilLabel];
-        });
-    });
+        .text(function (d) {
+          return d[coilLabel]
+        })
+    })
 
-    function updatePathData(data) {
-      let holeRadius = radius * holeRadiusProportion;
-      let segmentAngle = 360 / segmentsPerCoil;
-      let dataLength = data.length;
-      let coils = Math.ceil(dataLength / segmentsPerCoil); // number of coils, based on data.length / segmentsPerCoil
-      let coilWidth = chartRadius * (1 - holeRadiusProportion) / (coils + 1); // remaining chartRadius (after holeRadius removed), divided by coils + 1. I add 1 as the end of the coil moves out by 1 each time
+    function updatePathData (data) {
+      let holeRadius = radius * holeRadiusProportion
+      let arcAngle = 360 / arcsPerCoil
+      let dataLength = data.length
+      let coils = Math.ceil(dataLength / arcsPerCoil) // number of coils, based on data.length / arcsPerCoil
+      let coilWidth = chartRadius * (1 - holeRadiusProportion) / (coils + 1) // remaining chartRadius (after holeRadius removed), divided by coils + 1. I add 1 as the end of the coil moves out by 1 each time
 
-      data.forEach(function(d, i) {
-        let coil = Math.floor(i / segmentsPerCoil);
-        let position = i - coil * segmentsPerCoil;
-        let startAngle = position * segmentAngle;
-        let endAngle = (position + 1) * segmentAngle;
-        let startInnerRadius = holeRadius + i / segmentsPerCoil * coilWidth;
+      data.forEach(function (d, i) {
+        let coil = Math.floor(i / arcsPerCoil)
+        let position = i - coil * arcsPerCoil
+        let startAngle = position * arcAngle
+        let endAngle = (position + 1) * arcAngle
+        let startInnerRadius = holeRadius + i / arcsPerCoil * coilWidth
         let startOuterRadius =
           holeRadius +
-          i / segmentsPerCoil * coilWidth +
-          coilWidth * (1 - coilPadding);
-        let endInnerRadius = holeRadius + (i + 1) / segmentsPerCoil * coilWidth;
+          i / arcsPerCoil * coilWidth +
+          coilWidth * (1 - coilPadding)
+        let endInnerRadius = holeRadius + (i + 1) / arcsPerCoil * coilWidth
         let endOuterRadius =
           holeRadius +
-          (i + 1) / segmentsPerCoil * coilWidth +
-          coilWidth * (1 - coilPadding);
+          (i + 1) / arcsPerCoil * coilWidth +
+          coilWidth * (1 - coilPadding)
 
-        // vertices of each segment
-        d.x1 = x(startAngle, startInnerRadius);
-        d.y1 = y(startAngle, startInnerRadius);
-        d.x2 = x(endAngle, endInnerRadius);
-        d.y2 = y(endAngle, endInnerRadius);
-        d.x3 = x(endAngle, endOuterRadius);
-        d.y3 = y(endAngle, endOuterRadius);
-        d.x4 = x(startAngle, startOuterRadius);
-        d.y4 = y(startAngle, startOuterRadius);
+        // vertices of each arc
+        d.x1 = x(startAngle, startInnerRadius)
+        d.y1 = y(startAngle, startInnerRadius)
+        d.x2 = x(endAngle, endInnerRadius)
+        d.y2 = y(endAngle, endInnerRadius)
+        d.x3 = x(endAngle, endOuterRadius)
+        d.y3 = y(endAngle, endOuterRadius)
+        d.x4 = x(startAngle, startOuterRadius)
+        d.y4 = y(startAngle, startOuterRadius)
 
         // CURVE CONTROL POINTS
-        let midAngle = startAngle + segmentAngle / 2;
+        let midAngle = startAngle + arcAngle / 2
         let midInnerRadius =
-          holeRadius + (i + 0.5) / segmentsPerCoil * coilWidth;
+          holeRadius + (i + 0.5) / arcsPerCoil * coilWidth
         let midOuterRadius =
           holeRadius +
-          (i + 0.5) / segmentsPerCoil * coilWidth +
-          coilWidth * (1 - coilPadding);
+          (i + 0.5) / arcsPerCoil * coilWidth +
+          coilWidth * (1 - coilPadding)
 
         // MID POINTS, WHERE THE CURVE WILL PASS THRU
-        d.mid1x = x(midAngle, midInnerRadius);
-        d.mid1y = y(midAngle, midInnerRadius);
-        d.mid2x = x(midAngle, midOuterRadius);
-        d.mid2y = y(midAngle, midOuterRadius);
+        d.mid1x = x(midAngle, midInnerRadius)
+        d.mid1y = y(midAngle, midInnerRadius)
+        d.mid2x = x(midAngle, midOuterRadius)
+        d.mid2y = y(midAngle, midOuterRadius)
 
-        d.controlPoint1x = (d.mid1x - 0.25 * d.x1 - 0.25 * d.x2) / 0.5;
-        d.controlPoint1y = (d.mid1y - 0.25 * d.y1 - 0.25 * d.y2) / 0.5;
-        d.controlPoint2x = (d.mid2x - 0.25 * d.x3 - 0.25 * d.x4) / 0.5;
-        d.controlPoint2y = (d.mid2y - 0.25 * d.y3 - 0.25 * d.y4) / 0.5;
+        d.controlPoint1x = (d.mid1x - 0.25 * d.x1 - 0.25 * d.x2) / 0.5
+        d.controlPoint1y = (d.mid1y - 0.25 * d.y1 - 0.25 * d.y2) / 0.5
+        d.controlPoint2x = (d.mid2x - 0.25 * d.x3 - 0.25 * d.x4) / 0.5
+        d.controlPoint2y = (d.mid2y - 0.25 * d.y3 - 0.25 * d.y4) / 0.5
 
-        d.arcNumber = position;
-        d.coilNumber = coil;
-      });
+        d.arcNumber = position
+        d.coilNumber = coil
+      })
 
-      return data;
+      return data
     }
 
-    function x(angle, radius) {
+    function x (angle, radius) {
       // change to clockwise
-      let a = 360 - angle;
+      let a = 360 - angle
       // start from 12 o'clock
-      a = a + 180;
-      return radius * Math.sin(a * radians);
+      a = a + 180
+      return radius * Math.sin(a * radians)
     }
 
-    function y(angle, radius) {
+    function y (angle, radius) {
       // change to clockwise
-      let a = 360 - angle;
+      let a = 360 - angle
       // start from 12 o'clock
-      a = a + 180;
-      return radius * Math.cos(a * radians);
+      a = a + 180
+      return radius * Math.cos(a * radians)
     }
 
-    function chartWH(r) {
-      return r * 2;
+    function chartWH (r) {
+      return r * 2
     }
   }
 
-  chart.radius = function(value) {
-    if (!arguments.length) return radius;
-    radius = value;
-    return chart;
-  };
+  chart.radius = function (value) {
+    if (!arguments.length) return radius
+    radius = value
+    return chart
+  }
 
-  chart.holeRadiusProportion = function(value) {
-    if (!arguments.length) return holeRadiusProportion;
-    holeRadiusProportion = value;
-    return chart;
-  };
+  chart.holeRadiusProportion = function (value) {
+    if (!arguments.length) return holeRadiusProportion
+    holeRadiusProportion = value
+    return chart
+  }
 
-  chart.segmentsPerCoil = function(value) {
-    if (!arguments.length) return segmentsPerCoil;
-    segmentsPerCoil = value;
-    return chart;
-  };
+  chart.arcsPerCoil = function (value) {
+    if (!arguments.length) return arcsPerCoil
+    arcsPerCoil = value
+    return chart
+  }
 
-  chart.coilPadding = function(value) {
-    if (!arguments.length) return coilPadding;
-    coilPadding = value;
-    return chart;
-  };
+  chart.coilPadding = function (value) {
+    if (!arguments.length) return coilPadding
+    coilPadding = value
+    return chart
+  }
 
-  chart.segmentLabel = function(value) {
-    if (!arguments.length) return segmentLabel;
-    segmentLabel = value;
-    return chart;
-  };
+  chart.arcLabel = function (value) {
+    if (!arguments.length) return arcLabel
+    arcLabel = value
+    return chart
+  }
 
-  chart.coilLabel = function(value) {
-    if (!arguments.length) return coilLabel;
-    coilLabel = value;
-    return chart;
-  };
+  chart.coilLabel = function (value) {
+    if (!arguments.length) return coilLabel
+    coilLabel = value
+    return chart
+  }
 
-  return chart;
+  return chart
 }
